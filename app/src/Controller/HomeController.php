5bbf855484547ad83aa37acd7387e63c609376fb
Meyer Timothée <timvameyer@gmail.com>
2024-01-30 12:12:27 +0100

    Site stable

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -6,12 +6,9 @@ use App\Entity\Ads;
 use App\Entity\User;
 use App\Entity\Image;
 use App\Entity\Equipment;
-use App\Entity\Reservation;
-use App\Form\ReservationType;
 use App\Repository\AdsRepository;
 use App\Repository\EquipmentRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -33,30 +30,15 @@ class HomeController extends AbstractController
     }
 
     #[Route('/detail/{id}', name: 'detail', methods: ['GET'])]
-    public function detail(int $id, Ads $ads, Request $request, EntityManagerInterface $entityManager): Response
+    public function detail(int $id, Ads $ads)
     {
         $ads = $this->em->getRepository(Ads::class)->findByIdWithInfos($id);
         $user = $this->em->getRepository(User::class)->find($ads->getUserId());
-
-        $reservation = new Reservation();
-        $form = $this->createForm(ReservationType::class, $reservation);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($reservation);
-            $entityManager->flush();
-
-            Response::HTTP_SEE_OTHER;
-        }
         // dd($ads);
         return $this->render('home/detail.html.twig', [
             'ads' => $ads,
             'equipments' => $ads->getEquipmentId(),
-            'user' => $ads->getUserId(),
-            'reservation' => $reservation,
-            'form' => $form,
+            'user' => $ads->getUserId()
         ]);
     }
-
-
 }
